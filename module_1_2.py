ff49bc40ef401788f6f7950aad0cb156dac8b13b
l7uBac <dima.pivnenko@gmail.com>
2024-08-01 21:29:03 +0500

    Add files via upload

---
+++ module_1_2.py
@@ -0,0 +1,5 @@
+completed_tasks = 12
+time_spent = 1.5
+course_name = 'Python'
+time_for_one_task = time_spent / completed_tasks
+print('курс:', course_name + ', ' 'всего задач:', str(completed_tasks) + ', ', 'затрачено часов:', str(time_spent) + ', ', 'среднее время выполнения', time_for_one_task, 'часа.')
