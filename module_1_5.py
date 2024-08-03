abc3a1193c5782dbaa7ec618eb8c8abde1175797
l7uBac <dima.pivnenko@gmail.com>
2024-08-03 16:56:04 +0500

    Add files via upload

--- module_1_5.py
+++ module_1_5.py
@@ -1,7 +1,7 @@
-immutable_var = 1, 'Джон', 2, 'Уик', 9 / 3, '9 ярдов', 5 > 2
+immutable_var = 1, ',white', 2, 'bytes', 9 / 3, '9 ярдов', 5 > 2
 print(immutable_var)
 # immutable_var[0] = 10 - строка вызвала ошибку, сделал ее комментарием. Ошибка, потому что нет изменяемых данных.
-mutable_list = ['9 с половиной ярдов'], 2024, 9 < 2, 5 - 2
+mutable_list = ['9 с половиной', 9.5], 2024, 9 < 2, 5 - 2
 print(mutable_list)
 mutable_list[0][0] = str(1988)
 print(mutable_list)
