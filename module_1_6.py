a2410bb44dff37c99ef31d534a07d8949bf6e74d
l7uBac <dima.pivnenko@gmail.com>
2024-08-04 20:58:28 +0500

    Add files via upload

--- module_1_6.py
+++ module_1_6.py
@@ -1,11 +1,12 @@
-my_dict = {'Маша': 1988, 'Дима': 1988, 'Анюта': 2019, 'Илья': 2009}
-print(my_dict)
-print(my_dict['Маша'])
-print(my_dict.get('папа'))
-my_dict.update({'бабушка': 1954,
-                'дедушка': 1953})
+my_dict = {'Маша': str(1988) + 'г.р.', 'Дима': str(1988) + 'г.р.', 'Анюта': str(2019) + 'г.р.',
+           'Илья': str(2009) + 'г.р.'}
+print('Словарь', my_dict)
+print('Маша,', my_dict['Маша'])
+print(my_dict.get('папа', 'Не существующее значение'))
+my_dict.update({'бабушка': str(1954) + 'г.р.',
+                'дедушка': str(1953) + 'г.р.'})
 a = my_dict.pop('Илья')
-print(a)
+print('Удаленное значение - Илья,', a)
 print(my_dict)
 my_set = {'dad', 'mam', 5 > 8, False, 1, 1, 2, 1, 2, 'dad', 'mam'}
 print(my_set)
