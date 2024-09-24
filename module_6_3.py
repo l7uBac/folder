80e1a6f8cba6b6e13d3d188c72724fcdbf8f1540
l7uBac <dima.pivnenko@gmail.com>
2024-09-24 19:01:49 +0500

    Add files via upload

--- module_6_3.py
+++ module_6_3.py
@@ -1,31 +1,39 @@
 
 class Horse:
-    x_distance = 0
-    sound = 'Frrr'
+
+    def __init__(self):
+        self.x_distance = 0
+        self.sound = 'Frrr'
 
     def run(self, dx):
         self.x_distance += dx
-        return self.x_distance
+
 
 class Eagle:
-    y_distance = 0
-    sound = 'I train, eat, sleep, and repeat'
+
+    def __init__(self):
+        self.y_distance = 0
+        self.sound = 'I train, eat, sleep, and repeat'
 
     def fly(self, dy):
         self.y_distance += dy
-        return self.y_distance
+
 
 class Pegasus(Horse, Eagle):
 
+    def __init__(self):
+        super().__init__()
+
+
     def move(self, dx, dy):
-        self.run(dx)
-        self.fly(dy)
+        super().run(dx)
+        super().fly(dy)
 
     def get_pos(self):
-        return (self.x_distance, self.y_distance) # возвращает текущее положение пегаса в виде кортежа - (x_distance, y_distance) в том же порядке.
+        return (self.x_distance, self.y_distance)
 
-    def voice(self): #- который печатает значение унаследованного атрибута sound.
-        super().sound()
+    def voice(self):
+        print(self.sound)
 
 p1 = Pegasus()
 
