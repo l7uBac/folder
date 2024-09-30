a0bd537945ff78a21cfd3d6a1942a4511375db77
l7uBac <dima.pivnenko@gmail.com>
2024-09-30 14:01:18 +0500

    Add files via upload

--- module_7_3.py
+++ module_7_3.py
@@ -1,6 +1,5 @@
 import re
-import string
-string.punctuation
+
 class WordsFinder:
 
     def __init__(self, *file_names):
@@ -9,25 +8,35 @@ class WordsFinder:
 
     def get_all_words(self):
         all_words = {}
-        for file_name in self.file_names:
-            with open(file_name, 'r', encoding='utf-8') as file:
-                for line in file:
-                    line1 = line.lower()
-                    for p in string.punctuation:
-                        if p in line1:
-                            line2 = line1.replace(p, '')
-                            all_words[self.file_names] = line2
+        for name in self.file_names:
+            with open(name, 'r', encoding='utf-8') as file:
+                text = file.read().lower()
+                for sym in [',', '.', '=', '!', '?', ';', ':', ' - ']:
+                    text = text.replace(sym, '')
+                    value = text.split()
+                    all_words[name] = value
         return all_words
 
+    def find(self, word):
+        for name, words in self.get_all_words().items():
+            if word in words:
+                index = words.index(word.lower())
+            else:
+                print('Слово не найдено')
+        return {name: index +1}
 
 
+    def count(self, word):
+        for name, words in self.get_all_words().items():
+            if word in words:
+                index = words.count(word.lower())
+            else:
+                print('Слово не найдено')
+        return {name: index}
 
 
-
-    # def find(self, word):
-    #
-    #
-    # def count(self, word):
 
 finder2 = WordsFinder('test_file.txt')
 print(finder2.get_all_words()) # Все слова
+print(finder2.find('TEXT')) # 3 слово по счёту
+print(finder2.count('teXT')) # 4 слова teXT в тексте всего
