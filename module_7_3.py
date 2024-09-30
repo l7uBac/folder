c10ee05676c8c0908df14b6fd11d069d4f179dde
l7uBac <dima.pivnenko@gmail.com>
2024-09-30 12:19:51 +0500

    Add files via upload

--- module_7_3.py
+++ module_7_3.py
@@ -1,5 +1,6 @@
 import re
-
+import string
+string.punctuation
 class WordsFinder:
 
     def __init__(self, *file_names):
@@ -8,15 +9,17 @@ class WordsFinder:
 
     def get_all_words(self):
         all_words = {}
-        for file_name in self.file.names:
-            with open(file_names, 'r', encoding='utf-8') as file:
+        for file_name in self.file_names:
+            with open(file_name, 'r', encoding='utf-8') as file:
                 for line in file:
                     line1 = line.lower()
-                    import string
-                    string.punctuation
                     for p in string.punctuation:
                         if p in line1:
                             line2 = line1.replace(p, '')
+                            all_words[self.file_names] = line2
+        return all_words
+
+
 
 
 
@@ -27,4 +30,4 @@ class WordsFinder:
     # def count(self, word):
 
 finder2 = WordsFinder('test_file.txt')
-print(finder2.file_names)
+print(finder2.get_all_words()) # Все слова
